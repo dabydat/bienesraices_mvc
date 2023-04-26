f52df9d904bf7bff14b008b32f69d49a552b0af6
David Gomez <alejandrogomez2510@gmail.com>
2023-04-25 23:47:31 -0400

    Adding:  .-Webpack module for compiling js files

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+import path from 'path'
+
+export default {
+    mode: 'development',
+    entry: {
+        map:'./src/js/map.js'
+    },
+    output:{
+        filename: '[name].js',
+        path: path.resolve('public/js')
+    }
+}
